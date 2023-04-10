fcc4cc88a0258d473d73d94b5f3f4be5127671dd
Rauf <arauftdm@gmail.com>
2023-04-10 15:30:25 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+//Abdul Rauf ---- Assignment2----Q.No1
+
+function OuterAdd(num) {
+    return function(x) {
+        return x + num;
+    };
+}
+
+let addNum = OuterAdd(4);
+console.log(addNum(11)); // Output: 15
+console.log(addNum(6)); // Output: 12
+//==============================================
+
+//Abdul Rauf ---- Assignment2----Q.No
+
+
+//====================================
+
+
+
+//Abdul Rauf ---- Assignment2----Q.No
+
+//====================================
+
+
+//Abdul Rauf ---- Assignment2----Q.No
+
+//====================================
+
+
+
+//Abdul Rauf ---- Assignment2----Q.No
+
+//====================================
+
+
+
+//Abdul Rauf ---- Assignment2----Q.No
+
+//====================================
