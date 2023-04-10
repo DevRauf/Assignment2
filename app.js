3b79d42772fb765ec3823d8aa029d84a7d5401d4
Rauf <arauftdm@gmail.com>
2023-04-10 16:33:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
-//Abdul Rauf ---- Assignment2----Q.No1
+//Abdul Rauf ---- Assignment2----------------------------------------Q.No1
 
-function OuterAdd(num) {
-    return function(x) {
-        return x + num;
-    };
-}
+// function OuterAdd(num) {
+//     return function(x) {
+//         return x + num;
+//     };
+// }
 
-let addNum = OuterAdd(4);
-console.log(addNum(11)); // Output: 15
-console.log(addNum(6)); // Output: 12
+// let addNum = OuterAdd(4);
+// console.log(addNum(11)); // Output: 15
+// console.log(addNum(6)); // Output: 12
+
 //==============================================
 
-//Abdul Rauf ---- Assignment2----Q.No
+//Abdul Rauf ---- Assignment2----------------------------------------Q.No2
+
+// function searchArray(arr, val) {
+//     // Base case: if the array is empty, return false (value not found)
+//     if (arr.length === 0) {
+//         return false;
+//     }
+
+//     // Recursive case: check if the first element of the array is the value we're searching for
+//     if (arr[0] === val) {
+//         return true;
+//     } else {
+//         // If the first element is not the value we're searching for, search the rest of the array recursively
+//         return searchArray(arr.slice(1), val);
+//     }
+// }
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let val = +prompt("Enter Num to Search")
+
+// if (searchArray(arr, val)) {
+//     console.log(`The value ${val} was found in the array!`);
+// } else {
+//     console.log(`The value ${val} was not found in the array.`);
+// }
+
 
 
 //====================================
 
 
 
-//Abdul Rauf ---- Assignment2----Q.No
+//Abdul Rauf ---- Assignment2----------------------------------------Q.No
 
 //====================================
 
 
-//Abdul Rauf ---- Assignment2----Q.No
+//Abdul Rauf ---- Assignment2----------------------------------------Q.No
 
 //====================================
 
 
 
-//Abdul Rauf ---- Assignment2----Q.No
+//Abdul Rauf ---- Assignment2----------------------------------------Q.No
 
 //====================================
 
